1315b8faf4955f3fa7ba7d3212f90c1a8cb98cb6
Andres <trevvieaamb@gmail.com>
2020-02-03 11:41:57 +0100

    Versión refactorizada

--- geometria/src/Circulo_AMB.java
+++ geometria/src/Circulo_AMB.java
@@ -1,10 +1,10 @@
 
-public class Circulo extends FiguraGeometrica {
+public class Circulo_AMB extends FiguraGeometrica_AMB {
 	private double radio;
 	//Constante pi
 	static final double PI = 3.1416;
 	
-	public Circulo(double r, String tipoFigura) {
+	public Circulo_AMB(double r, String tipoFigura) {
 		super(tipoFigura);
 		radio = r;
 	}
--- geometria/src/FiguraGeometrica_AMB.java
+++ geometria/src/FiguraGeometrica_AMB.java
@@ -1,9 +1,9 @@
 
-public abstract class FiguraGeometrica {
+public abstract class FiguraGeometrica_AMB {
 
 	protected String tipoFigura;
 
-	public FiguraGeometrica(String tipoFigura) {
+	public FiguraGeometrica_AMB(String tipoFigura) {
 		super();
 		this.tipoFigura = tipoFigura;
 	}
